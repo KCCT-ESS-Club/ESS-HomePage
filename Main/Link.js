dd4cfd73c7b60a26fdd62a27929edf322fd8faae
r119310 <r119310@g.kobe-kosen.ac.jp>
2022-08-07 13:41:11 +0900

    remake of start animation

---
+++ Main/Link.js
@@ -0,0 +1,15 @@
+function aboutJPLink() {
+    window.location.href = "../About/index.html";
+}
+
+function newsJPLink() {
+    window.location.href = "../News/index.html";
+}
+
+function accessJPLink() {
+    window.location.href = "../Access/index.html";
+}
+
+function homeJPLink() {
+    window.location.href = "../Home/index.html";
+}
--- PageStartAnimation/Animation.js
+++ PageStartAnimation/Animation.js
@@ -1,19 +1,4 @@
-$(window).on('load',function(){
-    $("#splash-logo").delay(2100).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
-    
-    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-    $("#splash").delay(2500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
-    
-    $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
-    
-    });
-    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-    
-    //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
-    $('.splashbg').on('animationend', function() { 
-    //この中に動かしたいJSを記載
-    });
-    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
-    
-
-    });
+window.onload = function() {
+    const loadingAnime = document.getElementById('loading');
+    loadingAnime.classList.add("loaded");
+}
