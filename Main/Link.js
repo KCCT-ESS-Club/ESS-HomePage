84409d2a559ad9cef13e13203b618de927c6fdb9
r119310 <r119310@g.kobe-kosen.ac.jp>
2023-08-26 17:52:52 +0900

    made home smartphone page

--- Main/Link.js
+++ Main/Link.js
@@ -13,3 +13,15 @@ function accessJPLink() {
 function homeJPLink() {
     window.location.href = "../Home/index.html";
 }
+
+function kcctHPLink(){
+    window.location.href = "http://www.kobe-kosen.ac.jp/";
+}
+
+function kcctXLink() {
+    window.location.href = "https://twitter.com/kcct_ess"
+}
+
+function gitHubLink(){
+    window.location.href = "https://github.com/KCCT-ESS-Club/ESS-HomePage"
+}
